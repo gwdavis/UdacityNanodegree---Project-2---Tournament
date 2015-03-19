43e7fa160a17c62d2c778fbb07496dc97d6dd16b
gwdavis <computer@davisfamily.com>
2015-03-19 12:58:32 -0400

    Complete sort by OMPs in playerStandings
    - Add two new views to handle OMP
    - Update playerStandings to handle OMP
    - move dummy data to python method from PSQL
    - Move unused functions to the end

--- readme.md
+++ readme.md
@@ -1,15 +1,16 @@
-# Tournament v1.1- a Udacity Nano-Degree Project for Full Stack Foundations
-###Allow Tied Games
+# Tournament v1.2- a Udacity Nano-Degree Project for Full Stack Foundations
+###v1.2 adds: Distingquish ties in standings
 March 2015 by Gary Davis
 
-Tournament is small python backend that implements an SQL database using postgresql to store players and determine the winner for a Swiss-Style tournament (see: http://en.wikipedia.org/wiki/Swiss-system_tournament).  Methods/functions are provided that can clear the database, add players, record matches, display results and show matchings for the next round.  If there are an odd number of players, a bye will be included in the players matchings.
+Tournament is small python backend that implements an SQL database using postgresql to store players and determine the winner for a Swiss-Style tournament (see: http://en.wikipedia.org/wiki/Swiss-system_tournament).  Methods/functions are provided that can clear the database, add players, record matches, display results and show matchings for the next round.  If there are an odd number of players, a bye will be included in the players matchings.  Matches can be won or tied.  Points are awarded to players as win:1, loss: -1, tie: 0. Standings are based on 
+total points for each player and in the case of a tie, the sum of the total points for each of their opponents, or Opponent Match Points ("OMP")
 
 ###Requirements:
 * Python
 * postgresql
 * psql 9.3.6
 
-For the class, we installed a virtual machine using VirtualBox and Vagrant and pulled the necessary setup from GIT (see: https://www.udacity.com/wiki/ud197/install-vagrant)
+For the Udacity Nano-Degree class, we installed a virtual machine using VirtualBox and Vagrant and pulled the necessary setup from GIT (see: https://www.udacity.com/wiki/ud197/install-vagrant)
 
 ###Files:
 * tournament.sql - the postresql model of the database
@@ -39,7 +40,9 @@ For the class, we installed a virtual machine using VirtualBox and Vagrant and p
 ```ShellSession
 > cd /vagrant/tournament
 ```
-* The command line of the virtual machine (see above) we will set up the SQL database by opening PSQL and running the database model:  
+* From the command line of the virtual machine (see above) we will set up the SQL database by opening PSQL
+and running the database model.  This only needs to be done once. NOTE however that it will delete all existing 
+data in a database called 'tournament'. 
 ```ShellSession
 > psql  
 ```
@@ -47,39 +50,48 @@ For the class, we installed a virtual machine using VirtualBox and Vagrant and p
 ``` 
 > .\i tournament.sql
 ```
-
-From here you will have database with a few players and matches already added to the database for testing purposes. 
-The test routines were provided by Udacity and can be run from the vagrant terminal prompt:
+To exit PSQL:
+```
+> \q
+```
+The test routines were provided by Udacity and can be run from the vagrant terminal prompt. Be sure
+that you are in the directory /vagrant/tournament per the direction at the start of this section:
 ```ShellSession
 > ./tournament_test.py
 ```
-or from within python:
+or:
 ```ShellSession
 > python tournament_test.py
 ```
-####Methods/Functions included in tournament.py:
-* connect() - connects to the database from python
+####Methods (i.e. Functions) included in tournament.py:
 * deleteMatches() - delete all matches from the database
 * deletePlayers() - delete all players from teh database
 * countPlayers() - provides a count of the players
 * registerPlayer(name) - register a players name
-* playerStandings() - lists the current standing of the players including player ids
-* reportMatch(winner, loser) - report match results using player ids
+* playerStandings() - lists the current standing of the players including player ids, points, number 
+						of matches played and the sum of the points of their opponents
+* reportMatch(player1, player2, point1, points2) - report match results using player ids and points for each
+				player as win: 1, loss: -1 and tie: 0. 
 * swissPairings() - provides the pairs for the next round of matches
 
-To use the functions from python, run the python interpreter and import the necessary modules:
+To use the functions from python, run the python interpreter (or iPython) and import the necessary modules:
 ```ShellSession
 > python
 >>> import psycopg2
 >>> from tournament import *
->>> connect()
 ```
-The methods/functions can be called individually e.g.:
+The methods/functions can then be called individually e.g.:
 ```python
 >>> playerStandings()
-[(20, 'Fluttershy', 1L, 1L), (19, 'Twilight Sparkle', 1L, 1L), (21, 'Applejack', 0L, 1L), (22, 'Pinkie Pie', 0L, 1L)]
+[(1, 'Henry the VIII', 2L, 2L, Decimal('-2')), (5, 'Julius Ceasar', 1L, 2L, Decimal('-2')), (3, 'Richard the Lion Hearted', 0L, 2L, Decimal('1')), (6, 'Aristotle', 0L, 2L, Decimal('0')), (4, 'Louis the XIV', -1L, 2L, Decimal('0')), (2, 'Catherine the Great', -2L, 2L, Decimal('3'))]
+
 ```
-The first number in each tuple is the player id follwed by play name, number of wins and number of losses.  The 'L' after the numbers shows the formatting used for the number (i.e. long)
+The first number in each tuple is the player id follwed by play name, points, number of matches played and the sum of the points of their opponents which is used to decide on ties in the rankings.  The 'L' after the numbers shows the formatting used for the number (i.e. long).  The Decimal('1') format for opponent points snuck in there and I have no idea why but it works for sorting.
+
+A description of each of the functions can be obtain from the python interpreter by entering "print function_name.__doc__":
+```python
+>>> print playerstandings.__doc__
+```
 
 
 
--- tournament.py
+++ tournament.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 #
-# tournament.py v1.1 -- implementation of a Swiss-system tournament
-# allow tied games
+# tournament.py v1.2 -- implementation of a Swiss-system tournament
+# distinguish player ties
 # a Udacity Nano-Degree Project for Full Stack Foundations
 # March 2015 by Gary Davis
 #
 
 import psycopg2
-import pprint
-
 
 def connect():
     """Connect to the PostgreSQL database for tournament.py.
@@ -64,170 +62,39 @@ def registerPlayer(name):
 
 
 def playerStandings():
-    """Returns a list of the players and their points records and number of
-    matches played, sorted by total points.
+    """Returns a list of the players and their points records, number of
+    matches played, and the sum of thier opponent's points
+    (Opponent Match Points or OMP)in order to settle tied results.
+    The list is sorted by total points and OMP.
 
     Returns:
-      A list of tuples, each of which contains (id, name, points, matches):
+      A list of tuples, each of which contains (id, name, points, matches, OMP):
         id: the player's unique id (assigned by the database)
         name: the player's full name (as registered)
         points: the sum of wins, losses and ties for the player
         matches: the number of matches the player has played
-    """
-    conn = connect()
-    c = conn.cursor()
-    # Join the players table and a view of the matches table.
-    # The view of the matches table (long_match_list) lists each player
-    # in column one and his opponent and results (i.e. there will be two
-    # enteries for each match).  Select the id, name and a sum of the
-    # points for each player.
-    # Sort by the wins .
-    c.execute(
-        # Summing the points column gives the total number of points
-        # Sounting the points column gives the total number of matches
-        # players table provides the name
-        # long_match_view table provides the points column as a 1, -1 or 0
-        "SELECT\
-            P_Id as id,\
-            player_name as name,\
-            COALESCE(SUM(points), 0) as points,\
-            count(points) as matches\
-        FROM players\
-        LEFT JOIN long_match_list ON P_Id = player\
-        GROUP BY P_Id\
-        ORDER BY points desc;")
-    result = c.fetchall()
-    conn.close()
-    return result
-
-
-# Create a method that returns the number of points earned for
-# a particular player and in this use case we will call him an
-# opponent as we want to get the summ of opponets points.
-def playerPoints(opponentID):
-    """Sum the total points for a player and return an integer
-
-    Args: opponent id"""
+        OMP: Opponent Match Points is the sum of the points earned in all
+            reported matches in order to decide ties.
+        """
 
     conn = connect()
     c = conn.cursor()
     c.execute(
-        """SELECT SUM(points)
-        FROM long_match_list
-        WHERE player = %s""", (opponentID,))
-    result = str(c.fetchone()[0])
-    conn.close
-    return result
-
-
-# Create a method that returns a list of opponents
-def opponentList(playerid):
-    """Output a list of opponents, at the moment,
-    this list is a list of tuples with one opponent ID
-    in each tuple.
-
-    Args: player ID"""
-
-    conn = connect()
-    c = conn.cursor()
-    c.execute("""SELECT opponent
-        FROM long_match_list
-        WHERE player = %s""", (playerid,))
-    temp_result = c.fetchall()
-    conn.close
-    # convert list of single element tuples to a list
-    # from  http://stackoverflow.com/a/716761/4671044
-    result = [j for i in temp_result for j in i]
-    return result
-
-
-# Create a method that returns the points earned by opponents of
-# a given player
-def opponentPoints(playerid):
-    """Output the sum of points earned by the opponents of a given player
-
-    Args: player id"""
-    conn = connect()
-    c = conn.cursor()
-    c.execute("""SELECT SUM(points)
-        FROM long_match_list
-        WHERE player IN
-            (SELECT opponent
-            FROM long_match_list as LML2
-            WHERE LML2.player = %s)""", (playerid,))
-    result = str(c.fetchone()[0])
-    conn.close
-    return result
-
-
-# Create a method that returns the points earned by opponents of
-# a list of players
-def listOpponentPoints():
-    """Output a list of tuples containing player id and 
-    the sum of opponents points"""
-    conn = connect()
-    c = conn.cursor()
-    c.execute(
-         """SELECT
-                player, sum(points) as sum_points,
-                sum(opp_points) as sum_opp_points
-            FROM long_match_list as MBP
-            LEFT JOIN
-               (SELECT
-                    MBP2.player as opponent,
-                    SUM(points) as opp_points
-                FROM long_match_list as MBP2
-                WHERE player IN
-                        (SELECT opponent
-                        FROM long_match_list as MBP3)
-                GROUP BY MBP2.player) as opp_list
-                ON MBP.opponent = opp_list.opponent
-            GROUP BY player
-            ORDER BY sum_points desc, sum_opp_points desc""")
-
+        """SELECT
+                p.id,
+                p.name,
+                p.points,
+                p.matches,
+                o.OMP
+            FROM playerPoints p
+            LEFT JOIN cumPoints o
+            ON p.id = o.id
+            ORDER BY p.points desc, o.OMP desc
+            """)
     result = c.fetchall()
+    # BUG_ALERT - the OMP output type in result is Decimal('n') where n is
+    # the number of points.  It sorts correctly but is odd.
     conn.close
-    return result
-
-print opponentPoints(1)
-print opponentPoints(2)
-print opponentPoints(3)
-print opponentPoints(4)
-print opponentPoints(5)
-print listOpponentPoints()
-
-
-# Create a method that produces standings sorted by points and
-# opponent-match-points ("OMP").  Consider:
-# Left join players to long_match_list on P_id, to this left join
-# long_match_list2 on opponentsID and P_ID, then sum opponents points2
-# grouped on opponents, then sum points grouped on p_id,
-# order by points and OMP
-def OMP_Standings():
-    """Returns a list of the players and their points records and number of
-    matches played, sorted by total points and in the case of a tie by
-    the sum of the points earned by their opponents, or opponent-match-points.
-
-    Returns:
-      A list of tuples, each of which contains (id, name, points, matches):
-        id: the player's unique id (assigned by the database)
-        name: the player's full name (as registered)
-        points: the sum of wins, losses and ties for the player
-        matches: the number of matches the player has played
-    """
-    conn = connect()
-    c = conn.cursor()
-    c.execute("""SELECT
-            P_Id as id,
-            player_name as name,
-            SUM(points) as points,
-            count(points) as matches
-        FROM players
-        LEFT JOIN long_match_list ON P_Id = player
-        GROUP BY P_Id
-        ORDER BY points desc;""")
-    result = c.fetchall()
-    conn.close()
     return result
 
 
@@ -270,20 +137,24 @@ def reportMatch(player1, player2, player1Result, player2Result):
 # reportMatch(4, 6, 0, 0)
 # print playerStandings()
 
+
 def previousMatch(player1, opponent1):
     """Determines if two players have previously played a match
+
+    Args:   player id
+            opponent id
 
     Returns TRUE or FALSE"""
 
     conn = connect()
     c = conn.cursor()
     # Simply count the number of matches between players using the
-    # long_match_list_view that lists all matches for each player in the 
+    # matches_by_player_view that lists all matches for each player in the 
     # first column (i.e. there will be two entries for each match in
-    # in the long_match_list)
+    # in the matches_by_player)
     c.execute(
         """SELECT count(case when opponent = %s then 1 end)
-        from long_match_list
+        from matches_by_player
         where player = %s""", (opponent1, player1))
     result = c.fetchone()
     conn.close()
@@ -302,10 +173,10 @@ def swissPairings():
 
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
-        id1: the first player's unique id
-        name1: the first player's name
-        id2: the second player's unique id
-        name2: the second player's name
+        id1:    the first player's unique id
+        name1:  the first player's name
+        id2:    the second player's unique id
+        name2:  the second player's name
     """
     # Count the number of participants and add a partipant: "Bye"
     # At this point in the development of the application the organizer
@@ -341,7 +212,7 @@ def swissPairings():
 
             if previousMatch(standings[0][0], standings[i][0]) is False:
                 pairings.append((standings[i][0], standings.pop(i)[1],
-                    standings[0][0], standings.pop(0)[1]))
+                                 standings[0][0], standings.pop(0)[1]))
                 break
             else:
                 continue
@@ -350,33 +221,159 @@ def swissPairings():
             break
     return pairings
 
-    
-    """ EXTRA CREDIT COMMENTS:
-    Should have tried for the extra credits from the start...  Can't
-    get enthused about redoing my tables...
-    To Handle Odd Players - Done above but does not automatically enter a
-        result for the bye match.  This must be entered manually as with all
-        other results.
-    To Handle Ties:
-        - Match Table Parameters - Id, P1, P2, P1 Result, P2 Result
-          so that one enters 1 for win, -1 of loss and 0 for tie
-        - Modify report match
-           - Add a test to sum the two results columns to raise an error
-             if the sum != to zero
-        - Modify long_match_list_view wins column to simply be
-          the sum of the P1 and P2 results
-        - Modify the unit test for the new inputs.
-    To Handle Ties in Ranking:
-        - For each player, sum of points, count of matches, list of opponents, 
-          each opponent sum of wins which is in the query or do another query.
-    To Handle Multiple Tournaments (preliminary thoughts):
-        - New Table - Tournaments
-        - Add Tournaments method
-        - Add registered Player to a tournament method
-        - Add Delete player method removes a player from a tournament
-        - Add Match results will add the tournament identifyier
-        - Add tournament parameter to all applicable methods.  Alternatively
-          consider creating an "active tournament" value in the tournament table
-          so that only one tournament can be active.  Another alternative is 
-          to have an active tournament for the session as a global variable.
+
+def dummyDatabase():
+    """Populates the tables with some dummy players and matches
+    These can be delete with the provided methods/functions"""
+
+    conn = connect()
+    c = conn.cursor()
+    c.execute("""
+        -- Populates the players table with some dummy players
+
+        INSERT INTO players(player_name) VALUES ('Henry the VIII');
+        INSERT INTO players(player_name) VALUES ('Catherine the Great');
+        INSERT INTO players(player_name) VALUES ('Richard the Lion Hearted');
+        INSERT INTO players(player_name) VALUES ('Louis the XIV');
+        INSERT INTO players(player_name) VALUES ('Julius Ceasar');
+        INSERT INTO players(player_name) VALUES ('Aristotle');
+
+        -- Populate the matches table with some dummy matches
+
+        INSERT INTO matches(P1, P2, P1_result, P2_result) VALUES (1, 2,1,-1);
+        INSERT INTO matches(P1, P2, P1_result, P2_result) VALUES (3,4,1,-1);
+        INSERT INTO matches(P1, P2, P1_result, P2_result) VALUES (5,6,0,0);
+        INSERT INTO matches(P1, P2, P1_result, P2_result) VALUES (1, 3,1,-1);
+        INSERT INTO matches(P1, P2, P1_result, P2_result) VALUES (5,2,1,-1);
+        INSERT INTO matches(P1, P2, P1_result, P2_result) VALUES (4,6,0,0);
+        """)
+    conn.commit()
+    conn.close()
+
+
+""" EXTRA CREDIT COMMENTS:
+    =====================
+To Handle Odd Players -     Done above but does not automatically enter a
+    result for the bye match.  This must be entered manually as with all
+    other results.
+To Handle Ties -            Done
+To Handle Ties in Ranking - Done
+To Handle Multiple Tournaments (preliminary thoughts) - 
+                            Not done
+    - New Table - Tournaments
+    - Add Tournaments method
+    - Add registered Player to a tournament method
+    - Add Delete player method removes a player from a tournament
+    - Add Match results will add the tournament identifyier
+    - Add tournament parameter to all applicable methods.  Alternatively
+      consider creating an "active tournament" value in the tournament table
+      so that only one tournament can be active.  Another alternative is 
+      to have an active tournament for the session as a global variable.
+"""
+
+
+
+
+
+
+""" NOTA BENE
+Following functions were used to develop ideas but are not used by the 
+application"""
+
+
+def playerStandings_old():
+    """Returns a list of the players and their points records and number of
+    matches played, sorted by total points.
+
+    Returns:
+      A list of tuples, each of which contains (id, name, points, matches):
+        id: the player's unique id (assigned by the database)
+        name: the player's full name (as registered)
+        points: the sum of wins, losses and ties for the player
+        matches: the number of matches the player has played
     """
+    conn = connect()
+    c = conn.cursor()
+    # Join the players table and a view of the matches table.
+    # The view of the matches table (matches_by_player) lists each player
+    # in column one and his opponent and results (i.e. there will be two
+    # enteries for each match).  Select the id, name and a sum of the
+    # points for each player.
+    # Sort by the wins .
+    c.execute(
+        # Summing the points column gives the total number of points
+        # Sounting the points column gives the total number of matches
+        # players table provides the name
+        # long_match_view table provides the points column as a 1, -1 or 0
+        """SELECT
+            P_Id as id,
+            player_name as name,
+            COALESCE(SUM(points), 0) as points,
+            count(points) as matches
+        FROM players
+        LEFT JOIN matches_by_player ON P_Id = player
+        GROUP BY P_Id
+        ORDER BY points desc;""")
+    result = c.fetchall()
+    conn.close()
+    return result
+
+
+# Create a method that returns the number of points earned for
+# a particular player and in this use case we will call him an
+# opponent as we want to get the summ of opponets points.
+def playerPoints(opponentID):
+    """Sum the total points for a player and return an integer
+
+    Args: opponent id"""
+
+    conn = connect()
+    c = conn.cursor()
+    c.execute(
+        """SELECT SUM(points)
+        FROM matches_by_player
+        WHERE player = %s""", (opponentID,))
+    result = str(c.fetchone()[0])
+    conn.close
+    return result
+
+
+# Create a method that returns a list of opponents
+def opponentList(playerid):
+    """Output a list of opponents, at the moment,
+    this list is a list of tuples with one opponent ID
+    in each tuple.
+
+    Args: player ID"""
+
+    conn = connect()
+    c = conn.cursor()
+    c.execute("""SELECT opponent
+        FROM matches_by_player
+        WHERE player = %s""", (playerid,))
+    temp_result = c.fetchall()
+    conn.close
+    # convert list of single element tuples to a list
+    # from  http://stackoverflow.com/a/716761/4671044
+    result = [j for i in temp_result for j in i]
+    return result
+
+
+# Create a method that returns the points earned by opponents of
+# a given player
+def opponentPoints(playerid):
+    """Output the sum of points earned by the opponents of a given player
+
+    Args: player id"""
+    conn = connect()
+    c = conn.cursor()
+    c.execute("""SELECT SUM(points)
+        FROM matches_by_player
+        WHERE player IN
+            (SELECT opponent
+            FROM matches_by_player as LML2
+            WHERE LML2.player = %s)""", (playerid,))
+    result = str(c.fetchone()[0])
+    conn.close
+    return result
+
